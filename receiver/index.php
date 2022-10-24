00936871ec8421cf2a154923eef61b70b2cfaa34
Legi4ch <sorokin.oleg@gmail.com>
2022-10-24 10:04:04 +0300

    update webhooks receiver

--- receiver/index.php
+++ receiver/index.php
@@ -5,12 +5,6 @@ require "../classes/User.php";
 require "../classes/MessageParser.php";
 require_once "../config/Config.php";
 
-
-$data = json_decode(file_get_contents('php://input'), true);
-file_put_contents('/var/www/primevpn/support-bot/tmp/log.txt', '$data: ' . print_r($data, true) . "\n", FILE_APPEND);
-
-
-
 try {
     $receiver = new TgReceiver(file_get_contents('php://input'));
     $user = new User($receiver->getId());
